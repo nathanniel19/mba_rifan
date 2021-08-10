ec9cc55ff6598ef532d0b4343ece4ff95b110f4c
Jonathan Juan <jjuand22@gmail.com>
2021-08-10 23:19:10 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p><b>RIFKI NUR FIKAR RAMADHAN</b></p>
+        <p>Telah melakukan pemeriksaan SWAB SARS CoV-2</p>
+        <p>PCR di Laboratorium Klinik Utama Taradita 48</p>
+        <p>dengan Nomor Pendaftaram: TRD - 01932 Pada</p>
+        <p>Tanggal: 2021-08-12 dengan hasil Negatif</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
