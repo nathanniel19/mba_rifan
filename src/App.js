dcb44f8be68edee0574b014b062e455c5b2b7d2d
Jonathan Juan <jjuand22@gmail.com>
2021-08-15 14:21:27 +0700

    test2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p><b>RIFKI NUR FIKAR RAMADHAN</b></p>
-        <p>Telah melakukan pemeriksaan SWAB SARS CoV-2</p>
-        <p>PCR di Laboratorium Klinik Utama Taradita 48</p>
-        <p>dengan Nomor Pendaftaram: TRD - 01932 Pada</p>
-        <p>Tanggal: 2021-08-12 dengan hasil Negatif</p>
+        <div className="container">
+        <img src="https://cdn.mylab.co.id/images/mylab.png" alt="profile image" width="100" height="50"></img>
+        <p>Name	: Rifki Nur Fikar Ramadhan</p>
+        <p>NIK	: 3202322602930004</p>
+        <p>Date Of Birth	: 26 Feb 1993</p>
+        <p>Address	: Jl. Pondok kelapa selatan no.14 rt.03 rw.05</p>
+        <p>Gender	: Male</p>
+        <p>Mobile Phone	:6281218295001</p>
+        <p>Examination Date	: 14 Aug 2021 20:25:34</p>
+        <p>Examination Result	: <b>Negative</b></p>
+        <p>Clinic Phatologist	: Dr. Naimatul Mahanani, Sp. PK</p><br/>
+        <p>This report has been verified by system, no sign needed</p>
+        </div>
       </header>
     </div>
   );
